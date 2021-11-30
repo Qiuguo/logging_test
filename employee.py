46578eb420b7a0761a01973e53a7431e44a42442
Qiuguo <qiuguo.shan@guanshantech.com>
2021-11-30 11:50:11 +0800

    Add files via upload

---
+++ employee.py
@@ -0,0 +1,27 @@
+import logging
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(filename='employee.log', level=logging.INFO,
+                    format='%(asctime)s:%(levelname)s:%(name)s:%(message)s')
+
+
+class Employee:
+    """A simple Employee Class"""
+
+    def __init__(self, first, last):
+        self.first = first
+        self.last = last
+
+        logger.info('Created Employee: {} - {}'.format(self.fullname, self.email))
+
+    @property
+    def email(self):
+        return '{}.{}@email.com'.format(self.first, self.last)
+
+    @property
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+
+
+emp_1 = Employee('John', 'Smith')
+emp_2 = Employee('Corey', 'Schafer')
